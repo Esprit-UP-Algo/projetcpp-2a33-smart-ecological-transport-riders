b058ec2df749f7d29d89c4234e8efa7f085fc371
chamsnasr411 <145756585+chamsnasr411@users.noreply.github.com>
2023-11-03 10:52:32 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main()
+#include "interface1.h"
+#include "employe.h"
+#include "mainwindow.h"
+#include "menu.h"
+int main(int argc, char *argv[])
 {
-    cout << "Hello " << endl;
-    return 0;
+    QApplication a(argc, argv);
+
+    employe E;
+    Connection c;
+
+
+    bool test=c.createconnect();
+    interface1 i;
+     MainWindow w;
+    if(test)
+    {w.show();
+        QMessageBox::information(nullptr, QObject::tr("database is open"),
+                    QObject::tr("connection successful.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+    else
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                    QObject::tr("connection failed.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
+    return a.exec();
 }
